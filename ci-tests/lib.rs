dfeb26372b255abe511a16f281240d24c79bc377
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-06-30 23:44:31 +0200

    Use doc_comment instead of doctest for testing for the moment

--- ci-tests/lib.rs
+++ ci-tests/lib.rs
@@ -1,1 +1,3 @@
-doc_comment::doctest!("../README.md");
+// I don't use extern here because it's not stable yet as it seems (rustc 1.44.1)
+#[doc_comment::doc_comment(include_str!("../README.md"))]
+extern {}
